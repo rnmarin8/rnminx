c27c27fc41f547e5500a088441052a56590d32bb
rnmarin8 <marinasron@gmail.com>
2025-01-21 14:18:07 +0800

    initial commit
    first bolt program

---
+++ project-bolt-vitejs-vite-yjeorvsm/project/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00a67e',
+        secondary: '#f3f4f6',
+      }
+    },
+  },
+  plugins: [],
+}
